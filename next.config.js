eddf6818766115ed42f7f7e9b1df7e150bc552cb
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-03-12 23:40:05 +0000

    Add redirect from home to weather page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/weather/2.7527/54.6641/",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
